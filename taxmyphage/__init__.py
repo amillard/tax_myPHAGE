dd75a21635020b9a1c31215a47da4fd4665e1252
TSP <thomassp@sund.ku.dk>
2024-08-14 12:45:28 +0200

    removed the PackageNotFoundError if run inside git repo without installation

--- taxmyphage/__init__.py
+++ taxmyphage/__init__.py
@@ -3,6 +3,9 @@ This module is used to set the version of the package.
 It uses the version information from the package metadata.
 """
 
-from importlib.metadata import version
+from importlib.metadata import version, PackageNotFoundError
 
-__version__ = version(__package__)
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    __version__ = 'dev version'
